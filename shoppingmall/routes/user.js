8640329cf1a2856c8c1787fa66db346995784dba
10Hyuna <kimha105@naver.com>
2023-06-23 00:34:47 +0900

    [delete] 공부하던 프로젝트 폴더 삭제

---
+++ shoppingmall/routes/user.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const router = express.Router();
+
+router.get('/', (req, res) => {
+    res.json([
+        {id: 1, content:'hello'}, 
+        {id: 2, content:'hello2'},
+        {id: 3, content: 'hello3'},
+    ])
+});
+
+router.post('/', (req, res) => {
+    res.json({
+        id: 1,
+        content: '라랄라',
+    })
+})
+
+router.delete('/api/post', (req, res) => {
+
+})
+
+module.exports = router;
