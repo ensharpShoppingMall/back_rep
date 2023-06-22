9e204f9559c136b1d1494cd9b78131b4b3631e8b
10Hyuna <kimha105@naver.com>
2023-06-23 01:32:58 +0900

    [add] 회원 관련 라우팅 파일 처리

---
+++ shoppingmall/routes/member.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const router = express.Router();
+
+router.get('/login', function(req, res){
+    
+});
+
+router.post('/login', function(req, res){
+
+});
+
+router.get('/join', function(req, res) {
+
+});
+
+router.post('/join', function(req, res) {
+
+});
+
+module.exports = router;
